06ba5020417f659d03bccaeac9f4ff8d8c9a8911
w68468 <shiftjandy.dyn@gmail.com>
2022-10-20 12:23:31 +0200

    mess

---
+++ Lab2.py
@@ -0,0 +1,8 @@
+#Zadanie 1
+a = int(input("Podaj swój wiek: "))
+if a < 4:
+    print("Wstęp bezpłatny")
+elif a > 4 and a < 18:
+    print("bilet kosztuje 10zł")
+else:
+    print("Bilet kosztuje 20zł")
