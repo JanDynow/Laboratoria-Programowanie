caae4d89c05a775803e29cc5af4698561e4a7c71
w68468 <shiftjanddy.dyn@gmail.com>
2022-11-03 13:26:44 +0100

    cm

---
+++ Lab4/Lab4_1.py
@@ -0,0 +1,7 @@
+L = ["ziemniaki", "jabłka", "chleb", "ser"]
+print(L)
+print(L[0], L[1])
+
+L[2] = "mleko"
+print(L)
+print(L[-1], L[-2])
---
+++ Lab4/Lab4_2.py
@@ -0,0 +1,8 @@
+zestaw_1 = []
+x = int(input("Podaj liczbę elementów listy: "))
+i = 1
+while i <= x:
+    y = input("Podaj element: ")
+    zestaw_1.append(y)
+    i += 1
+print(zestaw_1)
