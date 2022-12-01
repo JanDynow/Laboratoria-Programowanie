655c43aecb7b1ec90415c794a61429f842050fe2
w68468 <shiftjanddy.dyn@gmail.com>
2022-12-01 13:24:58 +0100

    cm

--- Lab6/Lab6_3.py
+++ Lab6/Lab6_3.py
@@ -1,3 +1,15 @@
+
 def function(*args):
     print(*args)
-function(16, 24, 55, 87,)
+    for i in args:
+        print(i)
+function(1,1.5,False,[2,6])
+
+def max(numer1,*args):
+    print(numer1,*args)
+    a = numer1
+    for z in args:
+        if z>a:
+            a = z
+    return a
+print(max(66,2,3,-5,32,44,22))
